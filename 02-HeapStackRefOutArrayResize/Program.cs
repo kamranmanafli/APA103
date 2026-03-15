f39a197e9f493b5c46be03332d06216d1548ef08
manafli09 <manaflik96@gmail.com>
2026-03-16 01:35:00 +0400

    02-HeapStackRefOutArrayResize

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class program
+{
+
+    static int[] ArrayResize(int[] numbers, params int[] newNumbers)
+    {
+        int[] result = new int[numbers.Length + newNumbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            result[i] = numbers[i];
+        }
+
+        for (int i = 0; i < newNumbers.Length; i++)
+        {
+            result[numbers.Length + i] = newNumbers[i];
+        }
+        return result;
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3 };
+
+        numbers = ArrayResize(numbers, 4, 5, 6, 7, 8, 9);
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
