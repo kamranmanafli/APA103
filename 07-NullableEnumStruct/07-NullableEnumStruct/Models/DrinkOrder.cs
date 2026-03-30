46dbf3666f9660796603c6c9b02221bd65545669
manafli09 <manaflik96@gmail.com>
2026-03-30 06:05:05 +0400

    07-NullableEnumStruct

---
+++ 07-NullableEnumStruct/07-NullableEnumStruct/Models/DrinkOrder.cs
@@ -0,0 +1,91 @@
+﻿using _07_NullableEnumStruct.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_NullableEnumStruct.Models
+{
+    public class DrinkOrder
+    {
+        public int OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public DrinkType Drink { get; set; }
+        public DrinkSize Size { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Price { get; set; }
+
+        public DrinkOrder(int orderNumber, string customerName, DrinkType drink, DrinkSize size)
+        {
+            OrderNumber = orderNumber;
+            CustomerName = customerName;
+            Drink = drink;
+            Size = size;
+            Status = OrderStatus.New;
+            Price = CalculatePrice();
+        }
+
+        public decimal CalculatePrice()
+        {
+            decimal price = 0;
+
+            switch (Drink)
+            {
+                case DrinkType.Coffee:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small: price = 3; break;
+                        case DrinkSize.Medium: price = 4; break;
+                        case DrinkSize.Large: price = 5; break;
+                    }
+                    break;
+
+                case DrinkType.Tea:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small: price = 2; break;
+                        case DrinkSize.Medium: price = 3; break;
+                        case DrinkSize.Large: price = 4; break;
+                    }
+                    break;
+
+                case DrinkType.Juice:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small: price = 4; break;
+                        case DrinkSize.Medium: price = 5; break;
+                        case DrinkSize.Large: price = 6; break;
+                    }
+                    break;
+
+                case DrinkType.Water:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small: price = 1; break;
+                        case DrinkSize.Medium: price = 1.5m; break;
+                        case DrinkSize.Large: price = 2; break;
+                    }
+                    break;
+                default:
+                    return 0;
+            }
+        }
+
+        public void UpdateStatus(OrderStatus newStatus)
+        {
+            Status = newStatus;
+            Console.WriteLine($"Sifaris #{OrderNumber} status: {newStatus}");
+        }
+
+        public void DisplayOrder()
+        {
+            Console.WriteLine($"Order No: {OrderNumber}");
+            Console.WriteLine($"Customer: {CustomerName}");
+            Console.WriteLine($"Drink: {Drink}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Status: {Status}");
+            Console.WriteLine($"Price: {Price} AZN");
+        }
+    }
+}
---
+++ 07-NullableEnumStruct/07-NullableEnumStruct/Program.cs
@@ -0,0 +1,60 @@
+﻿using _07_NullableEnumStruct.Enums;
+using _07_NullableEnumStruct.Models;
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        var order1 = new DrinkOrder(101, "Ali", DrinkType.Coffee, DrinkSize.Medium);
+        order1.DisplayOrder();
+
+        order1.UpdateStatus(OrderStatus.Preparing);
+        order1.UpdateStatus(OrderStatus.Ready);
+        order1.UpdateStatus(OrderStatus.Delivered);
+
+        var order2 = new DrinkOrder(102, "Leyla", DrinkType.Tea, DrinkSize.Large);
+        order2.DisplayOrder();
+        order2.UpdateStatus(OrderStatus.Ready);
+
+        var order3 = new DrinkOrder(103, "Vuqar", DrinkType.Juice, DrinkSize.Small);
+        order2.DisplayOrder();
+        order2.UpdateStatus(OrderStatus.Ready);
+
+        Console.WriteLine("DrinkType");
+        foreach (var item in Enum.GetValues(typeof(DrinkType)))
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("DrinkSize");
+        foreach (var item in Enum.GetValues(typeof(DrinkSize)))
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("OrderStatus");
+        foreach (var item in Enum.GetValues(typeof(OrderStatus)))
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine(DrinkType.Coffee.ToString());
+        Console.WriteLine(DrinkSize.Large.ToString());
+
+        DrinkType parsedDrink = (DrinkType)Enum.Parse(typeof(DrinkType), "Tea");
+        DrinkSize parsedSize = (DrinkSize)Enum.Parse(typeof(DrinkSize), "Medium");
+
+        Console.WriteLine(parsedDrink);
+        Console.WriteLine(parsedSize);
+
+        Console.WriteLine($"1-ci sifaris qiymeti: {order1.Price}");
+        Console.WriteLine($"2-ci sifaris qiymeti: {order2.Price}");
+        Console.WriteLine($"3-ci sifaris qiymeti: {order3.Price}");
+
+        decimal total = order1.Price + order2.Price + order3.Price;
+        Console.WriteLine($"Total Cost: {total}");
+
+
+    }
+}
