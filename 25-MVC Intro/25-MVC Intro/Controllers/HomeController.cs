d3b464e25f53a478db8cddfd435c8af88a32cb38
manafli09 <manaflik96@gmail.com>
2026-05-05 01:48:44 +0400

    25-MVC Intro

---
+++ 25-MVC Intro/25-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace _25_MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var student = new JsonResult(new { id = 1, name = "Ali", surname = "Quliyev" });
+
+            //return student;
+
+            return View("Index");
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return RedirectToAction(nameof(Index), "Product");
+        }
+
+        public ViewResult Error()
+        { 
+            return View();
+        }
+    }
+}
